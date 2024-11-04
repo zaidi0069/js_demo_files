194d4d3a59dcfc07e1e37340b6c771fb823e8365
zaidi0069 <93919683+zaidi0069@users.noreply.github.com>
2024-11-04 22:40:03 +0500

    Delete xx.java

--- xx.java
+++
@@ -1,9 +0,0 @@
-public class PrimeChecker {
-    public static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-}
